6597360f09781a67bdd313cac35cf3ddc13590b3
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-27 18:54:50 -0300

    adicionando heranca no cachorro e no gato

--- src/br/com/logica/zoologico/heranca/Cachorro.java
+++ src/br/com/logica/zoologico/heranca/Cachorro.java
@@ -1,9 +1,10 @@
 package br.com.logica.zoologico.heranca;
 
-public class Cachorro {
-	
-	
-	
-	
+import br.com.logica.zoologico.classes.Animal;
 
+public class Cachorro extends Animal {
+	
+	
+	
+	
 }
