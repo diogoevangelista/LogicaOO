9e9b56d3538e410ca15c182e7152a8e19f1b3701
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-27 15:54:35 -0300

    Intanciando novos objetos

---
+++ src/br/com/logica/zoologico/principal/Main.java
@@ -0,0 +1,25 @@
+package br.com.logica.zoologico.principal;
+
+import br.com.logica.zoologico.classes.Animal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Criando uma nova instacia do objeto.
+		Animal a1 = new Animal();
+		
+		a1.nome = "Jojo Todinho";
+		a1.especie = "gato";
+		a1.idade = 1;
+		
+		Animal a2 = new Animal();
+		// Atribuindo os valores às propriedades.
+		a2.nome = "Spike";
+		a2.especie = "Cachorro";
+		a2.idade = 3;
+		
+	
+	}
+	
+	
+}
