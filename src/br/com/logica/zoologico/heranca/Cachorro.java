5f6e59780ebf8f2eb5a60300ffe66a4d5c944031
Diogo Evangelista <68126@proway.treina>
2020-10-04 15:15:03 -0300

    adicionando cor de pelo

--- src/br/com/logica/zoologico/heranca/Cachorro.java
+++ src/br/com/logica/zoologico/heranca/Cachorro.java
@@ -1,10 +1,18 @@
 package br.com.logica.zoologico.heranca;
 
+
 import br.com.logica.zoologico.classes.Animal;
 
 public class Cachorro extends Animal {
-	
-	
+
+	public Cachorro(String nome, String especie, int idade, String corPelo) {
+		super(nome, especie, idade, corPelo);
+	}
+
+
+	public Cachorro(String nome) {
+		super(nome);
+	}
 	
 	
 }
--- src/br/com/logica/zoologico/principal/Main.java
+++ src/br/com/logica/zoologico/principal/Main.java
@@ -1,37 +1,25 @@
 package br.com.logica.zoologico.principal;
 
-import br.com.logica.zoologico.classes.Animal;
+import br.com.logica.zoologico.heranca.Cachorro;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//Criando uma nova instacia do objeto.
-		Animal a1 = new Animal("branco", "gato", 1);
 		
 		
-		Animal a2 = new Animal("Rex", "cachorro", 2);
+		Cachorro a2 = new Cachorro ("Rex", "cachorro", 2, "banco");
 		
-		Animal a3 = new Animal("branco", "asd", 2);
-		a3.setNome ("bicho");
-	
 		
-		System.out.println("Nome do animal é " + a1.getNome());
 		System.out.println("Nome do animal é " + a2.getNome());
 		
 		if(a2.isAdulto()) {
 			System.out.println("o cachorro é adulto");
 		}else {
 			System.out.println("o cachorro não é aduto");
-		}
-		if(a1.isAdulto()) {
-			System.out.println("o gato é adulto");
-		}else {
-			System.out.println("o gato não é aduto");
-		}
-	}
-	
 		
+	}
 	
 }
 	
-
+}
